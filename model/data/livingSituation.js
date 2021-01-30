04860929e9cd29d5213285f4996ebfc6e3fee9b3
Name Here <53023770+brendankhoury@users.noreply.github.com>
2021-01-30 13:58:29 -0800

    adding data

---
+++ model/data/livingSituation.js
@@ -0,0 +1,11 @@
+const living = {'Nursing home/residential care':1.5,
+'Home with more people than rooms':1.5,
+'Homeless shelter':2,
+'Prison/Jail':2,
+'Group home':2,
+'Rehab center':2,
+'None of these':4};
+
+module.exports = {
+    living: living
+}
